1dba37dfb2a623660928b3893266177cc51f9040
Devansh Dixit <devanshdixit01@gmail.com>
2022-10-01 21:25:03 +0530

    updated

--- Components/Header/index.jsx
+++ Components/Header/index.jsx
@@ -12,12 +12,12 @@ export default function Header() {
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                     <a href="https://flowbite.com" className="flex items-center">
                         <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
-                        <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                        <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Infield</span>
                     </a>
                     <div className="flex items-center lg:order-2">
                         <Toggle />
                         <a href="#" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Log in</a>
-                        <a href="#" className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">Get started</a>
+                        <a href="/getstarted" className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">Get started</a>
                         <button onClick={(e)=>{
                         e.preventDefault();
                         setShowMenu(!showMenu);
---
+++ pages/getstarted.js
@@ -0,0 +1,13 @@
+import Onborading from "../Components/Onboarding";
+import Header from '../Components/Header'
+import Footer from "../Components/Footer";
+
+export default function GetStarted() {
+    return (
+      <>
+      <Header />
+      <Onborading />
+      <Footer />
+      </>
+      )
+    }
