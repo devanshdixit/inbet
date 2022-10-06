9b705e3fc8085aa46595155a7d13634ee3a17127
Devansh Dixit <devanshdixit01@gmail.com>
2022-10-06 18:18:50 +0530

    updated

--- pages/getstarted.js
+++ pages/getstarted.js
@@ -1,11 +1,17 @@
 import Onborading from "../Components/Onboarding";
 import Header from '../Components/Header'
 import Footer from "../Components/Footer";
+import Head from "next/head";
 
 export default function GetStarted() {
-    return (
-      <>
+  return (
+    <>
+      <Head>
+        <title>Inbet - GetStarted</title>
+        <meta name="description" content="Best betting platform" />
+        <link rel="icon" href="/logow.png" />
+      </Head>
       <Onborading />
-      </>
-      )
-    }
+    </>
+  )
+}
--- pages/register.js
+++ pages/register.js
@@ -1,13 +1,19 @@
 import Header from '../Components/Header'
 import Footer from "../Components/Footer";
 import Register from '../Components/Register';
+import Head from 'next/head';
 
 export default function GetStarted() {
     return (
-      <>
-      <Header />
-      <Register />
-      <Footer />
-      </>
-      )
-    }
+        <>
+            <Head>
+                <title>Inbet - Register</title>
+                <meta name="description" content="Best betting platform" />
+                <link rel="icon" href="/logow.png" />
+            </Head>
+            <Header />
+            <Register />
+            <Footer />
+        </>
+    )
+}
