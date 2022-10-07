6e4737c39a1fbfa1ed9b0f256c49e5c7ad809891
Devansh Dixit <devanshdixit01@gmail.com>
2022-10-07 20:42:22 +0530

    updated

--- Components/Firebase/index.js
+++ Components/Firebase/index.js
@@ -14,7 +14,6 @@ const firebaseConfig = {
     measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
   };
 
-// Initialize Firebase
 export const firebase = initializeApp(firebaseConfig);
 export const auth = getAuth();
 export const database = getFirestore(firebase);
